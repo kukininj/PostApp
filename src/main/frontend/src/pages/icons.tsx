80396725c73ee2b1abe0297bc78f91f27cac3168
Łukasz Niedzielski <luk20001122@gmail.com>
2023-04-18 16:47:01 +0200

    front: rewrite `Navbar` & `icons.tsx`

--- src/main/frontend/src/pages/icons.tsx
+++ src/main/frontend/src/pages/icons.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react';
-export const notifications = (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bell-fill" viewBox="0 0 16 16">
-        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
-    </svg>
-);
-export const person = (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
-        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-    </svg>
-);
-export const search = (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-    </svg>
-);
-export const location = (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
-    </svg>
-);
+import { Link } from 'react-router-dom';
+
+export const IconLink: React.FC<{ icon: string, to: string }> = ({ icon, to }) => {
+    return (
+        <Link className="btn btn-info rounded-circle flex-shrink-0 d-grid justify-content-center align-content-center shadow" style={{aspectRatio:"1/1"}} to={to}>
+            <i className={`bi ${icon}`}></i>
+        </Link>
+    )
+}
--- src/main/frontend/src/pages/navbar.tsx
+++ src/main/frontend/src/pages/navbar.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import * as Icons from './icons';
+import { IconLink } from './icons';
 
 enum UserRole {
     Anonymous,
@@ -11,45 +11,47 @@ enum UserRole {
 const UserIcon: React.FC<{ role: UserRole }> = ({ role }) => {
     switch (role) {
         case UserRole.Anonymous:
-            return (<Link to="/login"> {Icons.person} </Link>);
+            return (<IconLink to='/login' icon='bi-box-arrow-in-right' />);
         case UserRole.User:
-            return (<Link to="/profile"> {Icons.person} </Link>);
+            return (<Link className="h-auto" to="/profile"> <i className="bi bi-person"></i> </Link>);
         case UserRole.Admin:
-            return (<Link to="/profile"> {Icons.person} </Link>);
+            return (<Link className="h-auto" to="/profile"> <i className="bi bi-person"></i> </Link>);
     }
 }
 
-const PortableMenu: React.FC<{}> = () => {
+const PortableMenu: React.FC<{ className: string, style: React.CSSProperties }> = ({ className, style }) => {
     const [role, setRole] = React.useState(UserRole.Anonymous);
 
     return (
-        <div className={"portable-menu"}>
+        <div className={className} style={style}>
             <UserIcon role={role} />
-            {role != UserRole.Anonymous && Icons.notifications}
+            {role != UserRole.Anonymous && <i className="bi bi-bell"></i>}
         </div>
     );
 }
 
 const Navbar: React.FC<{}> = () => {
     return (
-        <div className="navbar navbar-fixed-top">
-            <Link to="/">
-                <img className="logo navbar-brand" src="img/logo.svg" />
-            </Link>
-            <div className="search-bar hstack gap-3">
-                <div className="search-input">
-                    <input type="text" name="search-querry-input" />
-                </div>
-                <div className="vr" ></div>
-                <div className="class-select hstack">
-                    <div>{Icons.location}</div>
-                    <input type="text" name="area-input" />
-                </div>
-                <div className="submit-search">
-                    {Icons.search}
-                </div>
+        <div className="navbar navbar-expand-lg navbar-fixed-top justify-content-center text-dark">
+            <div className="d-flex flex-md-nowrap flex-wrap justify-content-between gap-3 px-3">
+                <Link className="d-inline order-0 logo navbar-brand" to="/">
+                    <img src="/images/logo-sm.svg"/>
+                </Link>
+                <form className="inline-flex my-auto order-md-1 order-last hstack gap-3 rounded-pill bg-light p-2 shadow" style={{ height: "75px" }}>
+                    <div className="search-input p-1">
+                        <input className="form-control fs-3 bg-transparent border-0 shadow-none" type="text" name="search-querry-input" placeholder="Masz coś na myśli?" />
+                    </div>
+                    <div className="vr" ></div>
+                    <div className="class-select hstack p-1">
+                        <div className="m-1"><i className="bi bi-geo-alt-fill"></i></div>
+                        <input className="form-control fs-3 bg-transparent border-0 shadow-none" type="text" name="area-input" placeholder="Kraków"/>
+                    </div>
+                    <button className="submit-search btn btn-info rounded-circle flex-shrink-0" style={{ height: "100%", aspectRatio: "1/1" }}>
+                        <i className="bi bi-search"></i>
+                    </button>
+                </form>
+                <PortableMenu className="hstack my-auto gap-3 order-2 portable-menu rounded-pill bg-light p-3 shadow" style={{ height: "75px" }} />
             </div>
-            <PortableMenu />
         </div>
     );
 }
