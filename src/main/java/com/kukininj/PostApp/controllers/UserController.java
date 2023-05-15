74e5fe08101243ac7e8f4d677ac403dd35b11305
Łukasz Niedzielski <luk20001122@gmail.com>
2023-05-15 09:40:22 +0200

    backend: update `UserController`

--- src/main/java/com/kukininj/PostApp/controllers/UserController.java
+++ src/main/java/com/kukininj/PostApp/controllers/UserController.java
@@ -1,47 +1,63 @@
 package com.kukininj.PostApp.controllers;
 
-import com.kukininj.PostApp.communication.messages.ErrorMessage;
-import com.kukininj.PostApp.communication.messages.IMessage;
+import com.kukininj.PostApp.models.User;
 import com.kukininj.PostApp.models.requestmodels.RegisterForm;
 import com.kukininj.PostApp.models.responsemodels.RegisterResponse;
 import com.kukininj.PostApp.models.responsemodels.SetProfilePictureResponse;
+import com.kukininj.PostApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Set;
+import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserService userService;
 
     @PostMapping(
             value = "/register",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    @ResponseBody
-    RegisterResponse
+    <T extends RegisterResponse>
+    ResponseEntity<RegisterResponse>
     register(
             @RequestBody RegisterForm form
-            ) {
-        System.out.println(form.email);
-        System.out.println(form.password);
-        System.out.println(form.firstname);
-        System.out.println(form.surname);
-        return RegisterResponse.Success;
+    ) {
+        try {
+            User user = userService.addUser(form.email, form.password, form.surname, form.firstname);
+            return ResponseEntity
+                    .ok(T.Success(user));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(T.Fail(e));
+        }
     }
 
     @PostMapping(
             value = "/user/setProfilePicture",
             consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}
     )
+    <T extends SetProfilePictureResponse>
     ResponseEntity<SetProfilePictureResponse>
     setProfilePicture(
             @RequestParam("picture") MultipartFile picture
     ) {
         // TODO: file storage functionality
         System.out.println(picture.getResource().getFilename());
-        return ResponseEntity.ok(SetProfilePictureResponse.Success);
+        return ResponseEntity.ok(T.Success());
+    }
+
+    @GetMapping(
+            value = "/user/getActiveUsers"
+    )
+    ResponseEntity<List<User>>
+    getActiveUsers() {
+        return ResponseEntity.ok(userService.getActiveUsers());
     }
 }
