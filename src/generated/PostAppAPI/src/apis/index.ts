746d49c1f7febe4fbf337f74d8b7401584b410c8
Łukasz Niedzielski <luk20001122@gmail.com>
2023-05-21 19:19:39 +0200

    maven: generate `openapi.json` & `PostAppAPI`

---
+++ src/generated/PostAppAPI/src/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChatControllerApi';
+export * from './PostControllerApi';
+export * from './UserControllerApi';
---
+++ src/generated/PostAppAPI/src/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './RegisterForm';
+export * from './RegisterResponse';
+export * from './SetProfilePictureResponse';
+export * from './User';
