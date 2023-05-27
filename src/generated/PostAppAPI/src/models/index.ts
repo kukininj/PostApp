9a5f24d01b04ace260bf7d16e45e188400120360
Łukasz Niedzielski <luk20001122@gmail.com>
2023-05-27 13:49:53 +0200

    maven: regenerate `PostAppAPI`

--- src/generated/PostAppAPI/src/models/index.ts
+++ src/generated/PostAppAPI/src/models/index.ts
@@ -1,6 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './AddPostRequest';
 export * from './Picture';
+export * from './Post';
+export * from './PostCategory';
 export * from './RegisterForm';
 export * from './RegisterResponse';
 export * from './SetProfilePictureResponse';
