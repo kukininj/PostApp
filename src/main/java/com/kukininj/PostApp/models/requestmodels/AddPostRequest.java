f02c576e496fb8cfa2a29cc7898b62e2f7dbe778
Łukasz Niedzielski <luk20001122@gmail.com>
2023-05-27 13:42:37 +0200

    backend: impl `/post/create`, `/post/get/{post_id}`

---
+++ src/main/java/com/kukininj/PostApp/models/requestmodels/AddPostRequest.java
@@ -0,0 +1,10 @@
+package com.kukininj.PostApp.models.requestmodels;
+
+public class AddPostRequest {
+    public String title;
+    public String description;
+    public String price_str;
+    public String area;
+    public String category;
+    public Long picture_id;
+}
