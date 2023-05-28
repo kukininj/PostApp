00b17c66bfeb1880c3da358605873fdf554bb9b5
Łukasz Niedzielski <luk20001122@gmail.com>
2023-05-28 11:28:24 +0200

    backend: impl `/post/latest`, /post/filter'

--- src/main/java/com/kukininj/PostApp/controllers/PostController.java
+++ src/main/java/com/kukininj/PostApp/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.kukininj.PostApp.controllers;
 import com.kukininj.PostApp.models.Picture;
 import com.kukininj.PostApp.models.Post;
 import com.kukininj.PostApp.models.requestmodels.AddPostRequest;
+import com.kukininj.PostApp.models.requestmodels.FilteredFindRequest;
 import com.kukininj.PostApp.repository.PostRepository;
 import com.kukininj.PostApp.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.session.Session;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Filter;
 
 @RestController
 public class PostController {
@@ -36,8 +39,7 @@ public class PostController {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<Optional<Post>> addPost(
-            @RequestBody
-            AddPostRequest request
+            @RequestBody AddPostRequest request
     ) {
         BigDecimal price = new BigDecimal(request.price_str);
 
@@ -48,8 +50,37 @@ public class PostController {
                         price,
                         request.area,
                         request.category,
-                         // TODO: change to request.picture_id when Picture functionality is finished
+                        // TODO: change to request.picture_id when Picture functionality is finished
                         1L
+                )
+        );
+    }
+
+    @GetMapping(
+            value = "/post/latest",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<Post>> getLatest() {
+        return ResponseEntity.ok(
+                service.getLatest()
+        );
+    }
+
+    @PostMapping(
+            value = "/post/filter",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<Post>> getByFilter(
+            @RequestBody FilteredFindRequest filter
+    ) {
+        return ResponseEntity.ok(
+                service.filteredFind(
+                        filter.query,
+                        filter.category,
+                        filter.area,
+                        filter.minPrice,
+                        filter.maxPrice
                 )
         );
     }
--- src/main/java/com/kukininj/PostApp/repository/PostRepository.java
+++ src/main/java/com/kukininj/PostApp/repository/PostRepository.java
@@ -1,9 +1,18 @@
 package com.kukininj.PostApp.repository;
 
 import com.kukininj.PostApp.models.Post;
+import com.kukininj.PostApp.models.PostCategory;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#reference
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+    List<Post> findTop10ByOrderByIdDesc();
 }
