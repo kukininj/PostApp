ece0da453c346a5058c5302d6170bc27b39b1b0f
Łukasz Niedzielski <luk20001122@gmail.com>
2023-06-01 00:12:53 +0200

    backend: add @Repository to repositories, order by Added

--- src/main/java/com/kukininj/PostApp/repository/PostCategoryRepository.java
+++ src/main/java/com/kukininj/PostApp/repository/PostCategoryRepository.java
@@ -2,10 +2,12 @@ package com.kukininj.PostApp.repository;
 
 import com.kukininj.PostApp.models.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface PostCategoryRepository extends JpaRepository<PostCategory, UUID> {
     public Optional<PostCategory> findByName(String name);
 }
--- src/main/java/com/kukininj/PostApp/repository/TransactionRepository.java
+++ src/main/java/com/kukininj/PostApp/repository/TransactionRepository.java
@@ -4,10 +4,12 @@ import com.kukininj.PostApp.models.Transaction;
 import com.kukininj.PostApp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     @Query("FROM Transaction t WHERE t.id=?1 and ?2 in (t.client, t.merchant)")
     Optional<Transaction> findByIdAndClientOrMerchant(UUID transaction, User client);
